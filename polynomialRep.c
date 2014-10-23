0dc8e7bfcc01f2c4d72835d1cef635eaf553e4c9
Jesus Magana <maganazero5@gmail.com>
2014-10-22 22:45:38 -0700

    Have struct for Element

---
+++ polynomialRep.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+typedef struct element{
+
+	int value;
+	int exponent;
+} ELEMENT;
+
+void printElement(ELEMENT e1)
+{
+	printf("%dx^%d", e1.value, e1.exponent);
+}
+
+int main(void)
+{
+	ELEMENT e1;
+	e1.value = 5;
+	e1.exponent = 3;
+
+	printElement(e1);
+	return 0;
+}
