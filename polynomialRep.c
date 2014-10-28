98f3042dafaea411c8becf664df0e352fdb27f16
Jesus Magana <maganazero5@gmail.com>
2014-10-27 17:19:21 -0700

    Added init and delete for terms

--- polynomialRep.c
+++ polynomialRep.c
@@ -1,4 +1,6 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "assert.h"
 
 typedef struct element{
 	int value;
@@ -7,8 +9,22 @@ typedef struct element{
 
 typedef struct function{
 	int terms;
-	ELEMENT* elementArray;
+	ELEMENT** elementArray;
 } FUNCTION;
+
+ELEMENT* initElement(int inValue, int inExponent){
+	ELEMENT* e1 = malloc(sizeof(ELEMENT*));
+	assert(e1 != NULL);
+	e1->value = inValue;
+	e1->exponent = inExponent;
+	return e1;
+}
+
+void deleteElement(ELEMENT* e1){
+	e1->value = 0;
+	e1->exponent = 0;
+	free(e1);
+}
 
 void printElement(ELEMENT e1)
 {
@@ -18,7 +34,7 @@ void printElement(ELEMENT e1)
 void printFunction(FUNCTION f1)
 {
 	for(int i = 0; i < f1.terms;i++){
-		printElement(f1.elementArray[i]);
+		printElement(*f1.elementArray[i]);
 		(i == (f1.terms -1)) ? printf("") : printf(" + ");
 	}
 }
@@ -26,11 +42,11 @@ void printFunction(FUNCTION f1)
 int main(void)
 {
 	int size = 2;
-	ELEMENT elementArray[size];
+	ELEMENT* elementArray[size];
 	for(int i = 0; i < size; i++){
-		elementArray[i].value = 3;
-		elementArray[i].exponent = 4;
-		printElement(elementArray[i]);
+		elementArray[i] = initElement(3, 5);
+		printElement(*elementArray[i]);
+		deleteElement(elementArray[i]);
 	}	
 
 	return 0;
